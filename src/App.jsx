13fd0361ef470c3ac51193e7325c491a0900b807
ronithussain <xayaanhussain542@gmail.com>
2024-11-10 09:39:14 +0600

    tailwind css and daisy ui installed in react....

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl bg-rose-600 p-2 font-bold'>Vite + React</h1>
+      
+      
+    </>
+  )
+}
+
+export default App
